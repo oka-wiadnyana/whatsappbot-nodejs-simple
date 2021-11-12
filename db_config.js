5285ab1fc08bd866c4a1b352ee780ae0272093f6
Oka Wiadnyana <onsdee86@gmail.com>
2021-11-12 11:09:13 +0800

    db_config main setting

--- db_config.js
+++ db_config.js
@@ -1,10 +1,11 @@
 // inisisalisasi database
 const mysql = require("mysql");
 const db = mysql.createConnection({
-  host: "192.168.1.60",
-  user: "kom_tengah",
-  password: "hukum12345",
-  database: "sipp",
+  // sesuaikan konfigurasi dengan server
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "",
 });
 
 module.exports = db;
