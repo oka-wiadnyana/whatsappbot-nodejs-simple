101b5bc2bdedcb44c22bc985597d570efeaac60b
Oka Wiadnyana <onsdee86@gmail.com>
2022-04-22 07:33:45 +0800

    update wweb js

--- db_config.js
+++ db_config.js
@@ -2,9 +2,9 @@
 const mysql = require("mysql");
 const db = mysql.createPool({
   // sesuaikan konfigurasi dengan server
-  host: "192.168.1.60",
-  user: "kom_tengah",
-  password: "hukum12345",
+  host: "localhost",
+  user: "root",
+  password: "",
   database: "sipp",
 });
 
