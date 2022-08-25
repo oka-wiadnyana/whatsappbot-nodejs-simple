69ea3d0491311c6653153795ccf5d571ce0b600f
oka-wiadnyana <onsdee86@gmail.com>
2022-08-25 23:56:40 +0800

    fix bug select.js

--- select.js
+++ select.js
@@ -1,14 +1,9 @@
 const puppeteer = require("puppeteer");
-const moment = require("moment");
-
-let month = moment().format("MM");
-let year = moment().format("YYYY");
 
 let getSkorBulanan = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
   try {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-
     await page.goto("http://evaluasi.badilum.mahkamahagung.go.id/", {
       waitUntil: "networkidle0",
       timeout: 0,
@@ -21,10 +16,10 @@ let getSkorBulanan = async () => {
     );
     await page.select("select#kategori", "2");
     await page.select("select#kelas", "4");
-    await page.select("select#bulan_awal", month);
-    await page.select("select#tahun_awal", year);
-    await page.select("select#bulan_akhir", month);
-    await page.select("select#tahun_akhir", year);
+    await page.select("select#bulan_awal", "08");
+    await page.select("select#tahun_awal", "2022");
+    await page.select("select#bulan_akhir", "08");
+    await page.select("select#tahun_akhir", "2022");
 
     await page.evaluate(() =>
       document.querySelector(".btn.btn-sm.btn-success.m-r-5").click()
@@ -59,16 +54,16 @@ let getSkorBulanan = async () => {
     await browser.close();
     return responseMessage;
   } catch (error) {
-    responseMessage = "Koneksi ke EIS terputus";
+    responseMessage = `Koneksi ke eis terputus`;
+    await browser.close();
     return responseMessage;
   }
 };
 
 let getSkorTahunanKelas = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
   try {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-
     await page.goto("http://evaluasi.badilum.mahkamahagung.go.id/", {
       waitUntil: "networkidle0",
       timeout: 0,
@@ -119,16 +114,16 @@ let getSkorTahunanKelas = async () => {
     await browser.close();
     return responseMessage;
   } catch (error) {
-    responseMessage = "Koneksi ke EIS terputus";
+    responseMessage = `Koneksi ke eis terputus`;
+    await browser.close();
     return responseMessage;
   }
 };
 
 let getDataTahunan = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
   try {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-
     await page.goto("http://evaluasi.badilum.mahkamahagung.go.id/", {
       waitUntil: "networkidle0",
       timeout: 0,
@@ -161,13 +156,13 @@ let getDataTahunan = async () => {
     await browser.close();
     return responseMessage;
   } catch (error) {
-    responseMessage = "Koneksi ke EIS terputus";
+    responseMessage = `Koneksi ke eis terputus`;
+    await browser.close();
     return responseMessage;
   }
 };
 
-// getSkorTahunanKelas().then((res) => console.log(res));
-// console.log(year);
+// getDataTahunan().then((res) => console.log(res));
 module.exports = {
   getSkorBulanan,
   getSkorTahunanKelas,
